60f95a870db283227c109c0416f1efbd3be874df
Eugenio Guidetti <Eugenio Guidetti@10.169.162.114>
2013-01-22 16:27:37 +0100

    modifiche varie

--- SWIMv2Client/src/servlet/CompletaModificaAdmin.java
+++ SWIMv2Client/src/servlet/CompletaModificaAdmin.java
@@ -3,33 +3,32 @@ package servlet;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import session.GestoreAdminRemote;
-import utility.Comunicazione;
-import utility.Utilita;
 
-public class ModificaInfoAdminOld extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
+import session.GestoreAdminRemote;
+
+public class CompletaModificaAdmin extends HttpServlet {
 	
 	private RequestDispatcher dispatcher;
 	private Context context;
 	private GestoreAdminRemote gestoreAdmin;
+	private Pattern pattern;
+	private Matcher matcher;
 	private String nickname;
 	private String nome;
 	private String cognome;
 	private String password;
 	private String email;
-	private Pattern pattern;
-	private Matcher matcher;
+	
+	private static final long serialVersionUID = 1L;
 
-    public ModificaInfoAdminOld() {
+    public CompletaModificaAdmin() {
         super();
     }
 
@@ -49,38 +48,38 @@ public class ModificaInfoAdminOld extends HttpServlet {
 		email = request.getParameter("nEmail");
 		System.out.println("email: " + email);
 		/*try {
-			context = new InitialContext();
-			gestoreAdmin = (GestoreAdminRemote) context.lookup("GestoreAdminJNDI");
-			if(!email.equals("")){
-				pattern = Pattern.compile(Utilita.EMAIL_PATTERN);
-				matcher = pattern.matcher(email);
-				if(!matcher.find()) {
-					request.setAttribute("messaggio", Comunicazione.emailNonValida());
-				} else {
-					if(!gestoreAdmin.modificaEmail(nickname, email)) {
-						request.getSession().setAttribute
-							("messaggio", Comunicazione.erroreModificaInformazioni());
-					}
-				}
-			}
-			if(!nome.equals("")) {
-				if(!gestoreAdmin.modificaNome(nickname, nome)) {
+		context = new InitialContext();
+		gestoreAdmin = (GestoreAdminRemote) context.lookup("GestoreAdminJNDI");
+		if(!email.equals("")){
+			pattern = Pattern.compile(Utilita.EMAIL_PATTERN);
+			matcher = pattern.matcher(email);
+			if(!matcher.find()) {
+				request.setAttribute("messaggio", Comunicazione.emailNonValida());
+			} else {
+				if(!gestoreAdmin.modificaEmail(nickname, email)) {
 					request.getSession().setAttribute
 						("messaggio", Comunicazione.erroreModificaInformazioni());
 				}
 			}
-			if(!cognome.equals("")) {
-				if(!gestoreAdmin.modificaCognome(nickname, cognome)) {
-					request.getSession().setAttribute
-						("messaggio", Comunicazione.erroreModificaInformazioni());
-				}
+		}
+		if(!nome.equals("")) {
+			if(!gestoreAdmin.modificaNome(nickname, nome)) {
+				request.getSession().setAttribute
+					("messaggio", Comunicazione.erroreModificaInformazioni());
 			}
-			if(!password.equals("")) {
-				if(!gestoreAdmin.modificaPassword(nickname, password)) {
-					request.getSession().setAttribute
-						("messaggio", Comunicazione.erroreModificaInformazioni());
-				}
+		}
+		if(!cognome.equals("")) {
+			if(!gestoreAdmin.modificaCognome(nickname, cognome)) {
+				request.getSession().setAttribute
+					("messaggio", Comunicazione.erroreModificaInformazioni());
 			}
+		}
+		if(!password.equals("")) {
+			if(!gestoreAdmin.modificaPassword(nickname, password)) {
+				request.getSession().setAttribute
+					("messaggio", Comunicazione.erroreModificaInformazioni());
+			}
+		}
 		} catch (Exception e) {
 			request.getSession().setAttribute
 				("messaggio", Comunicazione.erroreModificaInformazioni());
