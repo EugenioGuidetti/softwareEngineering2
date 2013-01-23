73eae2793e22570856378385a89b137786bf08f5
Eugenio Guidetti <Eugenio Guidetti@10.169.162.114>
2013-01-23 08:59:14 +0100

    Aggiunta immagine di dafault user donna, modificata order by di una query dell'entità amicizia.

--- SWIMv2Server/ejbModule/entity/Amicizia.java
+++ SWIMv2Server/ejbModule/entity/Amicizia.java
@@ -22,7 +22,7 @@ import javax.persistence.*;
 			"FROM Amicizia a " +
 			"WHERE a.userRichiedente = :user " +
 			"AND a.momentoAccettazione IS NOT NULL " +
-			"ORDER BY a.momentoAccettazione"),
+			"ORDER BY a.userDestinatario.cognome, a.userDestinatario.nome"),
 	
 	@NamedQuery(name = "controllaAmici", query = 
 			"SELECT a " +
