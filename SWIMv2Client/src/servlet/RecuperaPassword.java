5837b145da04c52558a41872a4246cff80422315
Eugenio Guidetti <Eugenio Guidetti@EugenioAsusN53S>
2013-01-26 20:21:13 +0100

    non finisce più

--- SWIMv2Client/src/servlet/RecuperaPassword.java
+++ SWIMv2Client/src/servlet/RecuperaPassword.java
@@ -65,7 +65,6 @@ public class RecuperaPassword extends HttpServlet {
 			}
 			else{
 				request.setAttribute("messaggio", Comunicazione.confermaResetPassword());
-				System.out.println("nuova password: " + nuovaPassword);
 				inviaMail = true;
 			}
 
